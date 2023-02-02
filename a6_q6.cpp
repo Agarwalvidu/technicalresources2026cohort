6851666a4d19a31d96c48ce4a5916121344676f4
Agarwalvidu <119511621+Agarwalvidu@users.noreply.github.com>
2023-02-02 22:13:35 +0530

    Submitting Q6 of Assignment 1

---
+++ a6_q6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n ;
+    cin >> n ;
+    int sum=0;
+    while (n)
+    {
+        int p=n%10;
+        sum=(sum*10)+(p);
+        n/=10;
+    }
+    cout << sum << endl ;
+return 0;
+}
